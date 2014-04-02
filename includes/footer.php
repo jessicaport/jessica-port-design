b7a0572b21b9bad11b9eafbf7b8908ae86f5ada6
Jessica Port <jessicaport@gmail.com>
2014-04-02 12:16:37 -0700

    Footer- social media icons, sprite,  and copy

--- includes/footer.php
+++ includes/footer.php
@@ -3,15 +3,16 @@
 	<div class="contact">
 		<ul>
 		<li><h4>CONTACT:</h4> <a href="mailto:jp@jessicaportdesign.com">jp@jessicaportdesign.com</a></li>
-		<li><h4>LOCATION:</h4> Long Beach, CA</li>
+		<li><h4>LOCATION:</h4> Torrance, CA</li>
 		</ul>
 	</div>
 	
 	<div class="social-media">
-		<a href="http://twitter.com/jessica_port" class="twitter"></a>
-		<a href="http://www.linkedin.com/in/jessicaport" class="linkedin"></a>
-		<a href="http://instagram.com/jessica_dee?ref=badge" class="instagram"></a>
-		<a href="http://dribbble.com/jessicaport" class="dribbble"></a>
+		<a href="http://twitter.com/jessica_port" class="twitter icon"></a>
+		<a href="http://www.linkedin.com/in/jessicaport" class="linkedin icon"></a>
+		<a href="http://instagram.com/jessica_dee?ref=badge" class="instagram icon"></a>
+		<a href="http://dribbble.com/jessicaport" class="dribbble icon"></a>
+		<a href="http://github.com/jessicaport" class="github icon"></a>
 	</div>
 </div> <!--end footer-inner-->
 
